5ac26cf5488e4178bdad5eb0be6dd7022194331b
T3zz <t3zz@rbheromax.com>
2019-01-24 19:07:23 +0000

    Prepare SoundCloud API, we will auth users and manage external tracks with this

--- js/props.js
+++ js/props.js
@@ -3,3 +3,4 @@ localStorage.NPChannel = 'UVC Radio';
 localStorage.NPurl = 'https://mtmrad.io/radio/8010/live';
 localStorage.NPAPIurl = 'https://mtmrad.io/api/nowplaying/2';
 localStorage.HPurl = 'https://undergroundvampireclub.com';
+localStorage.ClientID = 'v4hEbr6QReyb81OAe82kyvhbvzPOES4V';
