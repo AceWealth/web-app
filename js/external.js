4f5f96b8f596e06fb32fb98c53a07163408e1697
T3zz <t3zz@rbheromax.com>
2019-01-24 05:12:45 +0000

    Remove debug code, used when transferring user to other platforms

--- js/external.js
+++ js/external.js
@@ -10,22 +10,16 @@ $("#cover_art").dblclick(function(){
 
 console.log('Opening link for: ' + qt);
 if (sessionStorage.NPSpotify === "null") {
-    console.log('No Spotify link found, trying YouTube...');
             if (sessionStorage.NPYouTube === "null") {
-                console.log('No YouTube link found, trying SoundCloud...');
                 if (sessionStorage.NPSoundCloud === "null") {
-                        console.log('No SoundCloud link found, using Search...');
                         window.open(search, '_blank');
                 } else {
-		console.log('Opening SoundCloud Link');
                 window.open(sessionStorage.SoundCloud, '_blank');
                 }
             } else {
-		console.log('Opening YouTube Link');
                 window.open(sessionStorage.NPYouTube, '_blank');
             }
     } else {
-	console.log('Opening Spotify Link');
         window.open(sessionStorage.NPSpotify, '_blank');
     		}
 	});
