9f75644bca341f37dfdfa11f2dfc7a0e602229b5
T3zz <t3zz@rbheromax.com>
2019-01-24 20:02:37 +0000

    Move init functions to proper index.js file

--- js/index.js
+++ js/index.js
@@ -1,2 +1,8 @@
+// Load elements on page
+window.onload = function () {
+        WebPlayer();
+        loadNowPlaying();
+        document.getElementsByClassName("Player")[0].style.opacity = "1";
+}
 // Force remove blur on Web Player
 $('.Player').css('filter', 'blur(0px)');
